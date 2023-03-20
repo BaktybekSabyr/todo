4e19b6499aba3775039b9a37c3c3d78b9b8efbc3
BaktybekSabyr <baktybeksabyr@gmail.com>
2023-03-20 16:36:03 +0600

    added todo model and in admin

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import ToDo
 
 def homepage(request):
     return render(request, "index.html")
 
 def test(request):
+    todo_list = ToDo.objects.all()
     return render(request, "test.html")
 
 def homework(request):
-    return render(request, "homework.html")
+    return render(request, "homework.html", {"todo_list": todo_list})
