1f6ece69ec2fd42e1689bd26d1f3ce3d65593322
BaktybekSabyr <baktybeksabyr@gmail.com>
2023-03-22 09:38:08 +0600

    added todo model and in admin

--- main/views.py
+++ main/views.py
@@ -8,5 +8,8 @@ def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
+def second(request):
+    return HttpResponse("test 2 page")
+
 def homework(request):
     return render(request, "homework.html")
